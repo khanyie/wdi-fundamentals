a1e3a8a6aa8cb927d4221b89185ab4ecbc299e01
khanyie <khanyapok@gmail.com>
2017-07-24 09:40:53 +0200

    finish

--- memory_game./js/main.js
+++ memory_game./js/main.js
@@ -1,5 +1,36 @@
-console.log("Up and running!");
-var cardOne= "King of Hearts";
-var cardTwo= "King of diamond";
-var cardThree= "Queen of diamond";
-var cardFour= "Queen of Hearts";
+//console.log("Up and running!");
+//var cardTwo = "King";
+//var cardFour = "Queen";
+
+//console.log("User flipped " + cardOne);
+
+
+
+
+var cards = ['queen' + 'queen'+ 'king' + 'king'];
+
+var cardsInPlay = [];
+
+cardsInPlay.push('queen');
+var cardOne = "queen";
+console.log("User flipped queen")
+
+
+cardsInPlay.push('king');
+var cardTwo = "king";
+
+     console.log("User flipped king")
+     if (cardsInPlay.length === 2 ) {
+       
+       if (cardsInPlay[0], cardsInPlay[1]);  
+
+     }
+if (cardOne === cardTwo) {
+
+ alert("You found a match!");
+}
+
+
+else {
+	alert("Sorry try again.");
+}
