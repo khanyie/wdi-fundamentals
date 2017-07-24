e9f6d06ca107148c9e01103929f50fa349cdbb2b
khanyie <khanyapok@gmail.com>
2017-07-24 15:53:38 +0200

    finish

--- memory_game./js/main.js
+++ memory_game./js/main.js
@@ -1,36 +1,39 @@
 //console.log("Up and running!");
-//var cardTwo = "King";
-//var cardFour = "Queen";
 
-//console.log("User flipped " + cardOne);
-
-
-
-
-var cards = ['queen' + 'queen'+ 'king' + 'king'];
-
+ 
+   console.log("User flipped " + cardOne);
+ var cards = ['queen' + 'queen' + 'king' + 'king'];
 var cardsInPlay = [];
 
-cardsInPlay.push('queen');
-var cardOne = "queen";
-console.log("User flipped queen")
-
-
-cardsInPlay.push('king');
-var cardTwo = "king";
-
-     console.log("User flipped king")
-     if (cardsInPlay.length === 2 ) {
-       
-       if (cardsInPlay[0], cardsInPlay[1]);  
-
-     }
-if (cardOne === cardTwo) {
-
- alert("You found a match!");
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		console.log("You found a match!");
+} else {
+	console.log("Sorry, try again.");
 }
+};
+console.log("User flipped " + cards[0]);
+cardsInPlay.push(cards[2]);
+if (cardsInPlay.length === 2) {
+	checkForMatch();
+};
 
 
-else {
-	alert("Sorry try again.");
-}
+ var cardOne = "queen";
+ cardsInPlay.push('queen');
+ console.log("User flipped queen")
+
+ var cardTwo = "king";
+ cardsInPlay.push('king');
+   console.log("User flipped king")
+   if (cardsInPlay.length === 2) {
+   	if (cardsInPlay[0], cardsInPlay[1]);
+   }
+
+    if (cardOne === cardTwo) {
+    
+      alert("You found a match!");
+  } 
+  else {
+      alert("Sorry, try again");
+  }
